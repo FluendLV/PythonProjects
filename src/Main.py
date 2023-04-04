2423b3664fe288b585f67ae0e57a5681a16cc7f3
Rick Scott <mahito12345@gmail.com>
2023-04-04 23:52:31 +0300

    added player choice

--- src/Main.py
+++ src/Main.py
@@ -20,9 +20,10 @@ FONT = pygame.font.SysFont("calibri", 32)
 
 # set game variables
 HEAP_SIZE = 25
-TURN = 1
+TURN = 0
 AI_TURN = 2
 HUMAN_TURN = 1
+AI_START = False
 
 # define functions
 def draw_text(text, font, color, x, y):
@@ -41,9 +42,13 @@ def clear_Area(stones):
     WINDOW.fill(WHITE, (100, 180, 400, 100))
     draw_stones(stones)
 
-def get_ai_move(stones):
+def get_ai_move(stones, currentTurn):
+    global AI_START
     print("Stones: ", stones)
     """function to get AI move using minimax algorithm"""
+    if currentTurn == 2 and stones == 25:
+        AI_START = True
+
     if stones < 4:
         return stones
     else:
@@ -51,7 +56,8 @@ def get_ai_move(stones):
         best_score = float('-inf')
         for move in range(1, 4):
             new_stones = stones - move
-            score = minimax(new_stones, False)
+            print(AI_START)
+            score = minimax(new_stones, not AI_START)
             if score > best_score:
                 best_score = score
                 best_move = move
@@ -81,10 +87,36 @@ def minimax(stones, is_maximizing):
 
 def nim_game():
     """function to play nim game"""
-    # initialize game variables
+    # initialize game variable
     stones = HEAP_SIZE
     winner = None
-    TURN = 1
+    TURN = 0
+
+    # menu screen loop
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                return
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                x, y = pygame.mouse.get_pos()
+                if x > 100 and x < 400 and y > 250 and y < 350:
+                    TURN = HUMAN_TURN
+                    break
+                elif x > 100 and x < 400 and y > 350 and y < 450:
+                    TURN = AI_TURN
+                    break
+
+        # draw menu screen
+        WINDOW.fill(WHITE)
+        draw_text("Nim Game", FONT, BLACK, 250, 50)
+        draw_text("Who goes first?", FONT, BLACK, 250, 150)
+        draw_text("Player", FONT, BLACK, 250, 300)
+        draw_text("AI", FONT, BLACK, 250, 400)
+        pygame.display.update()
+
+        if TURN != 0:
+            break
 
     # game loop
     running = True
@@ -100,15 +132,38 @@ def nim_game():
                     stones -= 1
                     clear_Area(stones)
                     pygame.display.update()
+                    if stones <= 0:
+                        winner = "HUMAN"
+                        WINDOW.fill(WHITE)
+                        draw_text(f"{winner} wins!", FONT, BLACK, WIDTH // 2, HEIGHT // 2)
+                        pygame.display.update()
+                        time.sleep(3)
+
                     TURN = AI_TURN
                     print(stones)
                 elif x > 200 and x < 300 and y > 350 and y < 450:
                     stones -= 2
+                    if stones <= 0:
+                        winner = "HUMAN"
+                        WINDOW.fill(WHITE)
+                        draw_text(f"{winner} wins!", FONT, BLACK, WIDTH // 2, HEIGHT // 2)
+                        pygame.display.update()
+                        time.sleep(3)
+                        stones = 25
+
                     clear_Area(stones)
                     TURN = AI_TURN
                     print(stones)
                 elif x > 300 and x < 400 and y > 350 and y < 450:
                     stones -= 3
+                    if stones <= 0:
+                        winner = "HUMAN"
+                        WINDOW.fill(WHITE)
+                        draw_text(f"{winner} wins!", FONT, BLACK, WIDTH // 2, HEIGHT // 2)
+                        pygame.display.update()
+                        time.sleep(5)
+                        stones = 25
+
                     clear_Area(stones)
                     TURN = AI_TURN
                     print(stones)
@@ -119,17 +174,17 @@ def nim_game():
             draw_text("AI is thinking...", FONT, BLACK, 250, 250)
             pygame.display.update()
             time.sleep(2)
-            move = get_ai_move(stones)
+            move = get_ai_move(stones, TURN)
             stones -= move
+            if stones <= 0:
+                    winner = "AI"
+                    WINDOW.fill(WHITE)
+                    draw_text(f"{winner} wins!", FONT, BLACK, WIDTH // 2, HEIGHT // 2)
+                    pygame.display.update()
+                    time.sleep(3)
             TURN = HUMAN_TURN
             print(stones)
 
-        # check for winner
-        if stones == 0:
-            if TURN == HUMAN_TURN:
-                winner = "AI"
-            else:
-                winner = "HUMAN"
 
         # draw game window
         WINDOW.fill(WHITE)
@@ -138,10 +193,8 @@ def nim_game():
         draw_text("1", FONT, BLACK, 150, 400)
         draw_text("2", FONT, BLACK, 250, 400)
         draw_text("3", FONT, BLACK, 350, 400)
-        if winner is not None:
-            draw_text(winner + " WINS!", FONT, BLACK, 250, 250)
 
-        elif TURN == HUMAN_TURN:
+        if TURN == HUMAN_TURN:
             draw_text("Your turn", FONT, BLACK, 250, 250)
         else:
             draw_text("AI is thinking...", FONT, BLACK, 250, 250)
